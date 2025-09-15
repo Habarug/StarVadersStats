74b06e7be9854e31bf0d8b845ca3392fdbb69f88
Habarug <ha@barug.no>
2025-09-15 16:21:19 +0200

    Cleanup init

--- src/StarVadersStats/main.py
+++ src/StarVadersStats/main.py
@@ -9,14 +9,20 @@ from . import _load
 class SVStats:
     def __init__(self, rundir=None):
         if not rundir:
-            if os.path.exists("data"):
-                dirs = [
-                    dir
-                    for dir in os.listdir("data")
-                    if os.path.isdir(os.path.join("data", dir))
-                ]
-                if len(dirs):
-                    self.rundir = os.path.join("data", dirs[0])
+            if not os.path.exists("data"):
+                os.mkdir("data")
+
+            dirs = [
+                dir
+                for dir in os.listdir("data")
+                if os.path.isdir(os.path.join("data", dir))
+            ]
+            if len(dirs):
+                self.rundir = os.path.join("data", dirs[0])
+            else:
+                raise ValueError(
+                    "No runs found in data folder. Please add 'history' folder, or supply a path"
+                )
 
         else:
             self.rundir = rundir
