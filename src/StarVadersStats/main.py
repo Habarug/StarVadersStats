804dee2d4ab377ab66c414615f3366acab9868e4
Habarug <ha@barug.no>
2025-09-21 13:13:52 +0200

    Add plot_runSuccess

--- src/StarVadersStats/main.py
+++ src/StarVadersStats/main.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from thefuzz.process import extractOne
 
 from . import _load
 
@@ -32,9 +33,45 @@ class SVStats:
         self._df_cards = self._load.get_df_cards()
         self._df_artifacts = self._load.get_df_artifacts()
 
-    ################################
+    ##############
+    # region PLOTS
+    ##############
+
+    def plot_runSuccess(self, pilot=None):
+        fig, ax = plt.subplots()
+
+        if not pilot:
+            df = self.df_runs
+        else:
+            pilot = extractOne(pilot, self._load.pilotDict)[0]
+            df = self.df_runs[self.df_runs["Pilot"] == pilot]
+
+        sct = ax.scatter(df.index, df["Difficulty"], c=df["Success"], marker="o")
+        cbar = plt.colorbar(sct, ax=ax)
+        cbar.set_ticks([0, 1, 2, 3])
+        cbar.set_ticklabels(["Failure", "False victory", "Act 4 death", "True victory"])
+
+        ax.set_yticks(df["Difficulty"].unique())
+        ax.set_yticklabels(
+            [
+                self._load.difficultyDict[int(i.get_position()[1])]
+                if (int(i.get_position()[1]) in self._load.difficultyDict)
+                else None
+                for i in ax.get_yticklabels()
+            ]
+        )
+
+        ax.grid()
+        ax.set_ylabel("Difficulty")
+        ax.set_xlabel("Run")
+
+        fig.set_size_inches(6, 3)
+
+        return fig, ax
+
+    #################################
     # region PROPERTIES AND SHORTCUTS
-    ################################
+    #################################
 
     @property
     def carddb(self):
